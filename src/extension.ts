bdfbb8f2eff05018f82ab358c4bc64c4e8201675
Brian Guo <BrianGuo@itri.org.tw>
2021-01-25 10:56:53 +0800

    change wabpack configs

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "demo-test" is now active!');
+	console.log('Congratulations, your extension "vdcode-demo" is now active123!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('demo-test.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('vdcode-demo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
