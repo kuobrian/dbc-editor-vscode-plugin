dfb1459453973d35681c1cc99ced2cfabfb2ae15
Brian Guo <BrianGuo@itri.org.tw>
2021-01-26 09:58:15 +0800

    expand tree view

--- src/extension.ts
+++ src/extension.ts
@@ -6,14 +6,18 @@ import {DataProvider, TreeViewItem} from "./treeview_dataprovider";
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "vdcode-demo" is now active123!');
+	console.log('Congratulations, your extension "vscode-demo" is now active123!');
 	let disposable = vscode.commands.registerCommand('vdcode-demo.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from demo-test!');
 	});
 	context.subscriptions.push(disposable);
 
-	const dataProvider = new DataProvider();
-	vscode.window.registerTreeDataProvider('TreeView', dataProvider);
+
+	
+	if (vscode.workspace.rootPath !== undefined) {
+		const dataProvider = new DataProvider(vscode.workspace.rootPath);
+		vscode.window.registerTreeDataProvider('TreeView', dataProvider);
+	}
 
 
 
@@ -23,36 +27,36 @@ export function activate(context: vscode.ExtensionContext) {
 				placeHolder: "your New TreeItem Id"
 			}) || "";
 			
-			if (itemId !== "") {
-				dataProvider.addItem(new TreeViewItem(itemId));
-			}
+			// if (itemId !== "") {
+			// 	dataProvider.addItem(new TreeViewItem(itemId));
+			// }
 
 			vscode.window.showInformationMessage("add Item ", itemId);
 		})
 	);
-	context.subscriptions.push(
-		vscode.commands.registerCommand("vscode-demo.edit_treeview", async (item: TreeViewItem) => {
-			const itemName = await vscode.window.showInputBox({
-				placeHolder: "your New TreeItem Name"
-			}) || "";
-			if (itemName !== "") {
-				dataProvider.editItem(item, itemName);
-			}
-			vscode.window.showInformationMessage("Edit item ", itemName);
+	// context.subscriptions.push(
+	// 	vscode.commands.registerCommand("vscode-demo.edit_treeview", async (item: TreeViewItem) => {
+	// 		const itemName = await vscode.window.showInputBox({
+	// 			placeHolder: "your New TreeItem Name"
+	// 		}) || "";
+	// 		if (itemName !== "") {
+	// 			dataProvider.editItem(item, itemName);
+	// 		}
+	// 		vscode.window.showInformationMessage("Edit item ", itemName);
 			
-		})
-	);
-	context.subscriptions.push(
-		vscode.commands.registerCommand("vscode-demo.delete_treeview", async (item: TreeViewItem) => {
-			const confirm = await vscode.window.showQuickPick(["delete", "cancel"], {
-				placeHolder: "Do you want to delete item?"
-			})
-			if (confirm === "delete") {
-				dataProvider.deleteItem(item);
-			}
-			vscode.window.showInformationMessage("delete item ");
-		})
-	);
+	// 	})
+	// );
+	// context.subscriptions.push(
+	// 	vscode.commands.registerCommand("vscode-demo.delete_treeview", async (item: TreeViewItem) => {
+	// 		const confirm = await vscode.window.showQuickPick(["delete", "cancel"], {
+	// 			placeHolder: "Do you want to delete item?"
+	// 		})
+	// 		if (confirm === "delete") {
+	// 			dataProvider.deleteItem(item);
+	// 		}
+	// 		vscode.window.showInformationMessage("delete item ");
+	// 	})
+	// );
 
 }
 
--- src/treeview_dataprovider.ts
+++ src/treeview_dataprovider.ts
@@ -1,58 +1,140 @@
+
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 
-class TreeViewItem extends vscode.TreeItem{
-    constructor(label: string, collapsibleState?: vscode.TreeItemCollapsibleState) {
-        super(label, collapsibleState);
-        this.contextValue = 'treeviewitem';
-    }
+class CANdb {
+    candbId: number;
+    network_nodes: string[];
+    messages: string[];
+    signals: string[];
+
+    constructor(id:number) {
+        this.candbId = id;
+        this.network_nodes = [];
+        this.messages = [];
+        this.signals = [];
+   }
 }
 
 
 
-class DataProvider implements vscode.TreeDataProvider<TreeViewItem>{
-    private dataStorage = [
-		new TreeViewItem('TreeItem-01'),
-		new TreeViewItem('TreeItem-02'),
-		new TreeViewItem('TreeItem-03'),
-    ];
+class TreeViewItem extends vscode.TreeItem{
+    constructor(
+        public readonly label: string,
+        private readonly version: string, 
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        public readonly command?: vscode.Command
+    ) {
+        super(label, collapsibleState);
+        this.tooltip = `${this.label}-${this.version}`;
+        this.description = this.version;
+    }
+
+    iconPath = {
+        light: path.join(__filename, '..', '..', 'assets', 'dependency.svg'),
+        dark: path.join(__filename, '..', '..', 'assets', 'dependency.svg')
+    };
+    contextValue = 'treeviewitem';
+}
+
+class DataProvider implements vscode.TreeDataProvider<TreeViewItem> {
+
+    private _onDidChangeTreeData: vscode.EventEmitter<TreeViewItem | undefined | void> = new vscode.EventEmitter<TreeViewItem | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<TreeViewItem | undefined | void> = this._onDidChangeTreeData.event;
+
+
+    constructor(
+        private workspaceRoot: string 
+    ) {}
+
+
+    private refresh() {
+        this._onDidChangeTreeData.fire();
+    }
+
+    private pathExists(p: string): boolean {
+		try {
+			fs.accessSync(p);
+		} catch (err) {
+			return false;
+		}
+
+		return true;
+    }
     
-    private eventEmitter = new vscode.EventEmitter<TreeViewItem | undefined | void>();
+    private getDepsInPackageJson(packageJsonPath: string): TreeViewItem[] {
+        if (this.pathExists(packageJsonPath) && (this.workspaceRoot !== undefined) ){
+            const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-    // onDidChangeTreeData?: vscode.Event<void | TreeViewItem | null | undefined> | undefined;
-    public get onDidChangeTreeData(): vscode.Event<TreeViewItem | undefined | void> {
-		return this.eventEmitter.event;
-	}
+            const toDep = (moduleName: string, version: string): TreeViewItem => {
+				if (this.pathExists(path.join(this.workspaceRoot, 'node_modules', moduleName))) {
+					return new TreeViewItem(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed);
+				} else {
+					return new TreeViewItem(moduleName, version, vscode.TreeItemCollapsibleState.None, {
+						command: 'extension.openPackageOnNpm',
+						title: '',
+						arguments: [moduleName]
+					});
+				}
+			};            
+            const deps = packageJson.dependencies
+				? Object.keys(packageJson.dependencies).map(dep => toDep(dep, packageJson.dependencies[dep]))
+				: [];
+			const devDeps = packageJson.devDependencies
+				? Object.keys(packageJson.devDependencies).map(dep => toDep(dep, packageJson.devDependencies[dep]))
+				: [];
+            return deps.concat(devDeps);
+        } else {
+            return [];
+        }
+    };
       
-
     getTreeItem(element: TreeViewItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
         return element;
     }
+
     getChildren(element?: TreeViewItem): vscode.ProviderResult<TreeViewItem[]> {
-        return Promise.resolve(this.dataStorage);
-    }
+        if (!this.workspaceRoot) {
+            console.log(this.workspaceRoot);
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
+        }
+        if (element) {
+            console.log(element.label);
+            return Promise.resolve(this.getDepsInPackageJson(path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')));
+        } else {
+            const packageJsonPath = path.join(this.workspaceRoot, "package.json");
+            if (this.pathExists(packageJsonPath)) {
+                return Promise.resolve(this.getDepsInPackageJson(packageJsonPath)); 
+            } else {
+				vscode.window.showInformationMessage('Workspace has no package.json');
+				return Promise.resolve([]);
+			}
 
-    private updateView() {
-        this.eventEmitter.fire();
-    }
-
-    public addItem(newItem:TreeViewItem){
-        this.dataStorage.push(newItem);
-        this.updateView();
-    }
-
-    public editItem(item: TreeViewItem, name:string){
-        const existItem = this.dataStorage.find(i => i.label === item.label);
-        if (existItem) {
-            existItem.label = name;
-            this.updateView();
         }
     }
 
-    public deleteItem(item: TreeViewItem) {
-        this.dataStorage = this.dataStorage.filter(i => i.label !== item.label);
-        this.updateView();
-    }
+
+
+    // public addItem(newItem:TreeViewItem){
+    //     this.dataStorage.push(newItem);
+    //     this.updateView();
+    // }
+
+    // public editItem(item: TreeViewItem, name:string){
+    //     const existItem = this.dataStorage.find(i => i.label === item.label);
+    //     if (existItem) {
+    //         existItem.label = name;
+    //         this.updateView();
+    //     }
+    // }
+
+    // public deleteItem(item: TreeViewItem) {
+    //     this.dataStorage = this.dataStorage.filter(i => i.label !== item.label);
+    //     this.updateView();
+    // }
 
 
 }
