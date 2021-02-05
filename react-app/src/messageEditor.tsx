4cd8be9984d42f56d6d45834233481e640fb7513
Brian Guo <BrianGuo@itri.org.tw>
2021-02-05 16:25:43 +0800

    templete for tab <Transmitters Table>

--- react-app/src/messageEditor.tsx
+++ react-app/src/messageEditor.tsx
@@ -2,17 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Container from 'react-bootstrap/Container';
 
 import {
-  Row, Col, Tabs, Tab, Badge,
+  Row, Col, Tabs, Tab,
   Table,
-  Navbar,
-  Nav,
-  NavDropdown,
-  InputGroup,
   Form,
-  FormControl,
   Button,
   Modal
  } from "react-bootstrap";
@@ -22,18 +16,22 @@ declare global {
       acquireVsCodeApi(): any;
     }
 }
-interface IRecipeProps {
+interface IMsgProps {
   msg?:any;
   allSignals?: any[];
 }
 
-interface IRecipeState {
+interface ISignalTableState {
   selectSignal: any[];
   show: boolean;
-  checkSelectSignal: boolean
 }
 
-class SelectSignalTable extends React.Component <IRecipeProps, IRecipeState> {
+interface ITransmittersTableState {
+  selectTransmitters: any[];
+  show: boolean;
+}
+
+class SelectSignalTable extends React.Component <IMsgProps, ISignalTableState> {
   constructor(props) {
     super(props);
     this.state = {selectSignal: [],
@@ -164,6 +162,123 @@ class SelectSignalTable extends React.Component <IRecipeProps, IRecipeState> {
     );
   }
 }
+
+
+
+class SelectTransmittersTable extends React.Component <IMsgProps, ITransmittersTableState> {
+  constructor(props) {
+    super(props);
+    this.state = {selectTransmitters: [],
+                  show: false };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+  }
+
+  handleClose () { this.setState({ show: false } );}
+  handleShow ()  { this.setState({ show: true }); }
+  handleSelectSignal (selectItem) {
+    if (! this.state.selectTransmitters.includes(selectItem)) {
+      const rows = [...this.state.selectTransmitters, selectItem];
+      this.setState({ selectTransmitters: rows}, () => {
+        console.log("Log: Add selectSignal", this.state.selectTransmitters.length);
+      }); 
+    } 
+  };
+
+
+  handleRemoveSpecificRow = (idx) => () => {
+    const rows = [...this.state.selectTransmitters];
+    rows.splice(idx, 1);
+    this.setState({ selectTransmitters: rows });
+  };
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row clearfix">
+            <div className="col-md-12 column">
+              <Table  striped bordered hover variant="dark">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Remove</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    { this.state.selectTransmitters.map((item, idx) => {
+                      return (
+                        <tr>
+                          <td>{item.name}</td>
+                          <td>{item.address}</td>
+                          <td>
+                            <Button variant="danger" onClick={this.handleRemoveSpecificRow(idx)} >
+                            Remove
+                            </Button>
+                          </td>
+                        </tr> 
+                      );})
+                    }
+
+                </tbody>
+              </Table >
+              <button onClick={this.handleShow} className="btn btn-primary">
+              Add ...
+              </button>
+              <Modal  size="lg"
+                            aria-labelledby="contained-modal-title-vcenter"
+                            centered
+                            show={this.state.show}
+                            onHide={this.handleClose}
+                            backdrop="static">
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Table  striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                              { 
+                                this.props.allSignals.map((item, idx) => {
+                                  if (! this.state.selectTransmitters.includes(item)) {
+                                    return (
+                                      <tr>
+                                        <td>{item.name} {this.state.selectTransmitters.includes(item)}</td> 
+                                        <td>{item.bitlength}</td>
+                                        <td>
+                                          <Button variant="primary" onClick={() => this.handleSelectSignal(item)}>
+                                          add
+                                          </Button>
+                                        </td>
+                                      </tr> 
+                                    );
+                                }
+                              })
+                              }
+                        </tbody>
+                    </Table >
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={this.handleClose}>
+                  quit
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 
 window.addEventListener('message', (event) =>{
     let vscode = window.acquireVsCodeApi();
@@ -296,37 +411,7 @@ window.addEventListener('message', (event) =>{
                         <SelectSignalTable msg={message}  allSignals={allSignals}/>
                     </Tab>
                     <Tab eventKey="transmitters" title="Transmitters">
-                      <Table striped bordered hover variant="dark">
-                          <thead>
-                            <tr>
-                              <th>Name</th>
-                              <th>Address</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>1</td>
-                                {Array.from({ length: 3 }).map((_, index) => (
-                                        <td key={index}>Table cell {index}</td>
-                                  ))
-                                }
-                            </tr>
-                            <tr>
-                              <td>2</td>
-                                {Array.from({ length: 3 }).map((_, index) => (
-                                        <td key={index}>Table cell {index}</td>
-                                  ))
-                                }
-                            </tr>
-                          </tbody>
-                      </Table>
-                      <Row>
-                          <Col>
-                              <Button variant="primary" onClick={t_handleShow}>
-                              View
-                              </Button>
-                          </Col>
-                        </Row>                    
+                        <SelectTransmittersTable msg={message}  allSignals={allSignals}/>
                     </Tab>
                     <Tab eventKey="receivers" title="Receivers" >
                       <Table striped bordered hover variant="dark">
